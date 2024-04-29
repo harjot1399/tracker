18280d0dfe0d93027f9efbb1b517eb3f228c6a7d
Harjot Singh <harjot1399@gmail.com>
2024-04-29 14:25:49 -0600

    some apis

--- tracker/api/urls.py
+++ tracker/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import JobsView
+from .views import JobsView, JobView
 
 urlpatterns = [path('<username>/jobs', JobsView.as_view(), name='jobs-view'),
+               path('<username>/jobs/<job_id>', JobView.as_view(), name='job-view')
                ]
